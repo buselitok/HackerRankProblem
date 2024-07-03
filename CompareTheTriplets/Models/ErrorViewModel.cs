3c878b1ec4e1c4ff9b2cc4d3a89ac7a546aa140a
Buse <buselitok@gmail.com>
2024-07-03 11:59:41 +0300

    [Deleted] CompareTheTriplets

--- CompareTheTriplets/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CompareTheTriplets.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
